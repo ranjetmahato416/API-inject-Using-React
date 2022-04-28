dd36ed106ea5b6d6226c788edf40df21c45bab2b
ranjit <ranjit@gmail.com>
2022-04-28 06:54:30 +0545

    First Commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar(){
+    return <div className="nav">
+        <h1>React Meals</h1>
+        <a>Home</a>
+        <a>Order</a>
+        <a>Contact Us</a>
+    </div>
+}
+export default Navbar
---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    return <div style={{backgroundColor: 'blue'}}>
+        <p style={{color:'white'}}>Designed and Developed by:</p>
+        <p style={{color:'white'}}>R.NPD.</p>
+    </div>
+}
+export default Footer
