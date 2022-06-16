ab3c71d2cf984b1b8ba30c12eada88a99e15074b
ranjit <ranjit@gmail.com>
2022-06-16 08:39:49 +0545

    Add exising files

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,11 @@ import './Navbar.css';
 function Navbar(){
     return <div className="nav">
         <h1>React Meals</h1>
-        <a>Home</a>
-        <a>Order</a>
-        <a>Contact Us</a>
+       <div className="options">
+         <a>Home</a>
+         <a>Order</a>
+         <a>Contact Us</a>
+       </div>
     </div>
 }
 export default Navbar
--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 function Footer(){
-    return <div style={{backgroundColor: 'blue'}}>
+    return <div style={{backgroundColor: 'gray'}}>
         <p style={{color:'white'}}>Designed and Developed by:</p>
-        <p style={{color:'white'}}>R.NPD.</p>
+        <p style={{color:'white '}}>R.NPD.</p>
     </div>
 }
 export default Footer
